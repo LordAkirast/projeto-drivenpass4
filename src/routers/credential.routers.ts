e71b7e6dfb320c2084786d21727e4482b97ed042
LordAkirast <darkwarish@gmail.com>
2024-05-12 21:49:37 -0300

    feat: configured routers for getCredentials

--- src/routers/credential.routers.ts
+++ src/routers/credential.routers.ts
@@ -3,11 +3,12 @@ import { createUser, deleteAllUsers, loginUser, logoutUser } from "../controller
 import express, { Express } from 'express';
 import cors from 'cors';
 import { validateUsersSchema, validateUserIDSchema } from "../middlewares/users.middleware";
-import { createCredential } from "../controllers/credentials.controller";
+import { createCredential, getCredentials } from "../controllers/credentials.controller";
 ////import de controllers
 
 const credentialRouter = Router()
 credentialRouter.post('/create', createCredential)
+credentialRouter.get('/get', getCredentials)
 //credentialRouter.post('/login', validateUsersSchema, loginUser)
 //credentialRouter.post('/logout', logoutUser)
 //credentialRouter.delete('/delete', deleteAllUsers)
