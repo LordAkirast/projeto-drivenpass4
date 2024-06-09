e9d8ae75e72352d37a349dd2079a0b0339be066e
LordAkirast <darkwarish@gmail.com>
2024-06-09 10:22:56 -0300

    feat: router credentialbyid

--- src/routers/credential.routers.ts
+++ src/routers/credential.routers.ts
@@ -3,10 +3,11 @@ import { createUser, deleteAllUsers, loginUser, logoutUser } from "../controller
 import express, { Express } from 'express';
 import cors from 'cors';
 import { validateUsersSchema, validateUserIDSchema } from "../middlewares/users.middleware";
-import { createCredential, getCredentials } from "../controllers/credentials.controller";
+import { createCredential, getCredentials, getCredentialByID } from "../controllers/credentials.controller";
 
 const credentialRouter = Router()
 credentialRouter.post('/create', createCredential)
 credentialRouter.get('/read', getCredentials)
+credentialRouter.get('/read/:id', getCredentialByID)
 
 export default credentialRouter;
